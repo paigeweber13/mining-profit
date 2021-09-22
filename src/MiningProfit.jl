6ab3eafcf6e5703268f2fb743b72a6132a17dc55
Paige Weber <paigeweber31@gmail.com>
2021-09-21 22:41:02 -0500

    adjusted miningpoolhub to be raw hashrate miningpoolhub reports hashrate in Kh/s, which is odd and somewhat misleading

--- src/MiningProfit.jl
+++ src/MiningProfit.jl
@@ -6,13 +6,17 @@ include("DBTools.jl")
 
 using Dates
 
+const KH_TO_MH = 1000
+
 function updateallstats() 
     cfg = Config.loadconfig()
 
     # miningpoolhub
     datestring = Dates.format(Dates.now(), "YYYY-mm-dd_HHMM")
-    mph_balance = ApiTools.miningpoolhub_getbalance(cfg["miningpoolhub"]["apikey"])
-    mph_hashrate = ApiTools.miningpoolhub_gethashrate(cfg["miningpoolhub"]["apikey"])
+    mph_balance = ApiTools.miningpoolhub_getbalance(
+        cfg["miningpoolhub"]["apikey"])
+    mph_hashrate = ApiTools.miningpoolhub_gethashrate(
+        cfg["miningpoolhub"]["apikey"]) * KH_TO_MH
     DBTools.insertminingdata("miningpoolhub", datestring, mph_hashrate, 
       mph_balance["confirmed"])
 end
