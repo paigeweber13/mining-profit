c8b8cff54cf58d1c492441c60a56644b0e716a50
Paige Weber <paigeweber31@gmail.com>
2021-09-21 23:51:30 -0500

    updateallstats: now gets data for flexpool

--- src/MiningProfit.jl
+++ src/MiningProfit.jl
@@ -25,6 +25,13 @@ function updateallstats()
     ezil_balance = ApiTools.ezil_getbalance(cfg["ezil"]["combinedwallet"])
     ezil_hashrate = ApiTools.ezil_gethashrate(cfg["ezil"]["combinedwallet"])
     DBTools.insertminingdata(db, "ezil", datestring, ezil_hashrate, ezil_balance)
+
+    # flexpool
+    flexpool_balance = ApiTools.flexpool_getbalance( cfg["flexpool"]["wallet"])
+    flexpool_hashrate = ApiTools.flexpool_gethashrate(
+        cfg["flexpool"]["wallet"])
+    DBTools.insertminingdata(db, "flexpool", datestring, flexpool_hashrate, 
+        flexpool_balance)
 end
 
 end # module
